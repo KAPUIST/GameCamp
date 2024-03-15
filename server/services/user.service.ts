7a4c0614cd773bd61a3a4019999fb155b18ed30e
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-03-15 19:55:28 +0900

    로깅 패키지 추가

--- server/services/user.service.ts
+++ server/services/user.service.ts
@@ -4,7 +4,7 @@ import userModel from "../models/user.model";
 //유저 조회
 export const getUserId = async (id: string, res: Response) => {
   const user = await userModel.findById(id);
-  res.status(201).json({
+  res.status(200).json({
     success: true,
     user,
   });
