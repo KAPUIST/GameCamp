efc7884375d35607995a64ae898c5842eca417a3
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-04-02 21:43:46 +0900

    Feat-Profile-ImageUpload
    개인 프로필 페이지 추가 Oauth 회원가입 기능
    개인 프로필 페이지에서 이미지 업로그 기능 추가
    로그아웃 기능 추가

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -5,7 +5,9 @@ import { ThemeProvider } from "./utils/theme-provider";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import { Providers } from "./Provider";
-
+import { SessionProvider } from "next-auth/react";
+import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
+import Loader from "./components/Loader/Loader";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
@@ -29,12 +31,19 @@ export default function RootLayout({
         className={`${poppins.variable} ${josefin.variable} bg-white bg-no-repeat dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300`}
       >
         <Providers>
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            {children}
-            <Toaster position="top-center" reverseOrder={false} />
-          </ThemeProvider>
+          <SessionProvider>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+              <Custom>{children}</Custom>
+              <Toaster position="top-center" reverseOrder={false} />
+            </ThemeProvider>
+          </SessionProvider>
         </Providers>
       </body>
     </html>
   );
 }
+
+const Custom: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { isLoading } = useLoadUserQuery({});
+  return <>{isLoading ? <Loader /> : <>{children}</>}</>;
+};
--- client/redux/store.ts
+++ client/redux/store.ts
@@ -13,11 +13,13 @@ export const store = configureStore({
     getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
-// call the load user function on every page load
-// const initializeApp = async () => {
-//   await store.dispatch(
-//     apiSlice.endpoints.loadUser.initiate({}, { forceRefetch: true })
-//   );
-// };
+const initializeApp = async () => {
+  await store.dispatch(
+    apiSlice.endpoints.refreshToken.initiate({}, { forceRefetch: true })
+  );
+  await store.dispatch(
+    apiSlice.endpoints.loadUser.initiate({}, { forceRefetch: true })
+  );
+};
 
-//initializeApp();
+initializeApp();
